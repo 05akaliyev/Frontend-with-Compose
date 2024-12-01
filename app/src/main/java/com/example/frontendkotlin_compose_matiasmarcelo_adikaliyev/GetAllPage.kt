5b253a64c3e736312a9c488c7a7071b69afe1a24
Matias-Marcelo <matiamarcelo756@gmail.com>
2024-12-01 23:36:35 +0100

    Fixed issue for GetAll

--- app/src/main/java/com/example/frontendkotlin_compose_matiasmarcelo_adikaliyev/GetAllPage.kt
+++ app/src/main/java/com/example/frontendkotlin_compose_matiasmarcelo_adikaliyev/GetAllPage.kt
@@ -18,7 +18,7 @@ import com.example.frontendkotlin_compose_matiasmarcelo_adikaliyev.nurses
 @Composable
 fun ShowAllNurses(){
 
-
+//getAll
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
